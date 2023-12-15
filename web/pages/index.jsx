1dafa245e027dfa59f9e364ec82ce3c12735883c
Zach Sents <zachsents@gmail.com>
2023-12-15 10:14:37 -0700

    Added live demo

--- web/pages/index.jsx
+++ web/pages/index.jsx
@@ -1,30 +1,78 @@
-import { Anchor, Button, Center, Stack, Text, Title } from "@mantine/core"
+import { Anchor, Button, Card, Center, Group, Stack, Text, ThemeIcon, Title } from "@mantine/core"
 import Header from "@web/components/Header"
+import classNames from "classnames"
 import Link from "next/link"
+import { useRouter } from "next/router"
+import { useEffect } from "react"
+import { TbLetterA, TbLetterB, TbRefresh } from "react-icons/tb"
 
 
 export default function LandingPage() {
 
+    const router = useRouter()
+    useEffect(() => {
+        if (router.isReady && !router.query.dv)
+            router.replace("?dv=a", undefined, { shallow: true })
+    }, [router.isReady, router.query.dv])
 
     return (
         <>
             <Header />
-            <Center className="w-screen min-h-screen p-xl">
-                <Stack className="items-center max-w-2xl">
-                    <Title order={1} className="text-center text-5xl">
-                        Dirt-cheap A/B testing for your website
-                    </Title>
-                    <Text className="text-center text-xl text-gray">
-                        Actually, it's not even cheap. It's free.
-                    </Text>
+            <Center className="w-screen min-h-screen p-xl mt-48 md:mt-0">
+                <Group className="items-start gap-20">
+                    <Stack className="items-start max-w-2xl">
+                        <Title order={1} className="text-4xl md:text-5xl" id="landing-headline">
+                            Dirt-cheap A/B testing for your website
+                        </Title>
+                        <Text className="text-xl text-gray" id="landing-subtitle">
+                            Actually, it's not even cheap. It's free.
+                        </Text>
+                        <Button className="mt-xl" component={Link} href="/login">
+                            Get Started Now
+                        </Button>
+                        <Text className="text-gray mt-20">Made by <Anchor href="https://twitter.com/Zach_Sents" target="_blank">Zach Sents</Anchor></Text>
+                    </Stack>
 
-                    <Button className="mt-xl" component={Link} href="/login">
-                        Get Started Now
-                    </Button>
+                    <Stack className="mt-10 min-w-[20rem]">
+                        <Title order={3}>Try it out!</Title>
 
-                    <Text className="text-gray mt-20">Made by <Anchor href="https://twitter.com/Zach_Sents" target="_blank">Zach Sents</Anchor></Text>
-                </Stack>
+                        <DemoVariantCard name="A" icon={TbLetterA} href="/?dv=a" />
+                        <DemoVariantCard name="B" icon={TbLetterB} href="/?dv=b" />
+                    </Stack>
+                </Group>
             </Center>
+
+            {/* eslint-disable-next-line @next/next/no-sync-scripts */}
+            <script src="https://dirt-ab.web.app/site-script.js" id="dirt-ab-site-script" data-site-id="qs7XuVsyTnpOVWDGwtnf" key="dirt-ab-site-script"></script>
         </>
+    )
+}
+
+
+function DemoVariantCard({ href, name, icon: Icon }) {
+
+    const router = useRouter()
+    const active = router.query.dv === name.toLowerCase()
+
+    return (
+        <Card component="a" href={href} withBorder
+            className={classNames("shadow-xs p-xl transition", {
+                "outline outline-2 outline-primary hover:outline-2": active,
+                "hover:scale-102 hover:shadow-sm": !active,
+            })}
+        >
+            <Group className="justify-between">
+                <Group>
+                    <ThemeIcon>
+                        <Icon />
+                    </ThemeIcon>
+                    <Text>
+                        Variant {name}
+                    </Text>
+                </Group>
+
+                <TbRefresh className="text-gray" />
+            </Group>
+        </Card>
     )
 }
